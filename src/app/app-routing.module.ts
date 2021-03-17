0b77b88c6f578ea89b386066e426de983bc303aa
aymen2782 <aymen.sellaouti@gmail.com>
2021-03-17 09:44:32 +0100

    route inint

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FirstComponent } from './components/first/first.component';
+import { TodoComponent } from './todo/todo/todo.component';
+import { ColorComponent } from './components/color/color.component';
+import { CvComponent } from './cv/cv/cv.component';
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: FirstComponent },
+  { path: 'cv', component: CvComponent },
+  { path: 'todo', component: TodoComponent },
+  { path: 'color', component: ColorComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
